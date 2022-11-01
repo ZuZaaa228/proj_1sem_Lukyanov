1ce25616e5ae755a4017e4f3848baf7eca6d4cb8
ZuZaaa228 <lukjanov-danilrambler.ru@yandex.ru>
2022-11-01 23:34:44 +0300

    add newline comm

--- PZ_5/PZ_1.py
+++ PZ_5/PZ_1.py
@@ -1,10 +1,17 @@
-def f(lst: list) -> int:
-    return sum(lst)  # Сложение всех чисел из списка с числовым рядом
+# Составить функцию, которая выполнит сумирование числового ряда
+def funcion_sum(numerical_series: list) -> int:
+    return sum(numerical_series)  # Сложение всех чисел из списка с числовым рядом
 
 
-lst = input("Введите числовой ряд.\nПример: 12 747 6236 \n").split()  # Создание списка с числовым рядом
+numerical_series = input("Введите числовой ряд.\nПример: 12 747 6236 \n").split()  # Создание списка с числовым рядом
 
-for index in range(len(lst)):
-    lst[index] = int(lst[index])  # форматирование каждлого элемента списка
+index = 0  # id элемента в списке
+while index < len(numerical_series):  # Обработчик исключений
+    while type(numerical_series[index]) != int:
+        try:
+            numerical_series[index] = int(numerical_series[index])
+        except ValueError:
+            numerical_series[index] = input("Введите целое число без лишних символов: ")
+    index += 1
 
-print(f(lst)) # Результат вызова функции
+print(funcion_sum(numerical_series))  # Результат вызова функции
--- PZ_5/PZ_2.py
+++ PZ_5/PZ_2.py
@@ -1,11 +1,41 @@
-def minmax(lst: list):
-    x = min(lst)  # Присвоение минимального числа в переменную
-    y = max(lst)  # Присвоение максимального числа в переменную
-    return f"x: {x} y: {y}"
+# Описать функцию minmax(X, Y) записывающую в переменную X минимальное, а в переменную Y максимальное из значений
+# Используя четыре вызова функции найти минимальное и максимальное из данных чисел A, B, C, D
+def minmax(x: int, y: int):
+    if x > y:
+        x, y = y, x  #x и y меняются местами
+        return x, y
+    return x, y
 
 
-A, B, C, D = int(input("Введите первое чмсло")), int(input("Введите второе чмсло")), int(
-    input("Введите третье чмсло")), int(input("Введите четвертое чмсло"))
-lst = [A, B, C, D]  # Создание списка из введенных чисел
+A, B, C, D = input("Введите первое число: "), input("Введите второе число: "), input("Введите третье число: "), input(
+    "Введите четвертое число: ")
 
-print(minmax(lst))  # Вывод результата
+while type(A) != int or type(B) != int or type(C) != int or type(D) != int:  #Обработчик исключений
+    if type(A) != int:
+        try:
+            A = int(A)
+        except ValueError:
+            A = input("Введите целое число A без лишних символов: ")
+    if type(B) != int:
+        try:
+            B = int(B)
+        except ValueError:
+            B = input("Введите целое число B без лишних символов: ")
+    if type(C) != int:
+        try:
+            C = int(C)
+        except ValueError:
+            C = input("Введите целое число C без лишних символов: ")
+    if type(D) != int:
+        try:
+            D = int(D)
+        except ValueError:
+            D = input("Введите целое число D без лишних символов: ")
+
+
+A, B = minmax(A, B)  # Первый вызов функции
+C, D = minmax(C, D)  # Второй вызов функции
+A, C = minmax(A, C)  # Третий вызов функции
+B, D = minmax(B, D)  # Четвертый вызов функции
+
+print(f"Min: {A}\nMax: {D}")
