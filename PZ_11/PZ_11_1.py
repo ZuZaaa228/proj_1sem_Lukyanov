b1ca14e2d3688c268592cf80595f34b2d7bf35ad
ZuZaaa228 <lukjanov-danilrambler.ru@yandex.ru>
2023-01-21 19:09:59 +0300

    final

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -11,11 +11,11 @@
 import random  # Импортируем модуль рандома
 
 
-def sequence(N) -> str:  # создаем функцию генерации последовательности
+def sequence(n) -> str:  # создаем функцию генерации последовательности
     line_sequence = []  # Объявляем пустой список
-    while N != 0:
+    while n != 0:
         line_sequence.append(str(random.randint(-100, 100)))
-        N -= 1
+        n -= 1
     line_sequence = " ".join(line_sequence)  # Записываем в строку список
     return line_sequence  # Возращаем строку с последовательностью
 
@@ -23,18 +23,20 @@ def sequence(N) -> str:  # создаем функцию генерации по
 # Создание и запись в файл 1
 file1 = open('file1.txt', 'w+', encoding="UTF-8")  # Открываем файл для записи
 all_elements = sequence(random.randint(1, 10))  # Все элементы файла 1
-min_index = str(all_elements.split().index(str(min([int(x) for x in all_elements.split()]))))  # Индекс минимального числа файла 1
-file1.write(all_elements)  #  Запись в файл 1
+min_index = str(
+    all_elements.split().index(str(min([int(x) for x in all_elements.split()]))))  # Индекс минимального числа файла 1
+file1.write(all_elements)  # Запись в файл 1
 file1.close()  # Закрываем файл
 
 # Создание и запись в файл 2
 file2 = open('file2.txt', 'w+', encoding="UTF-8")
 
 file2_elements = sequence(random.randint(1, 10))  # Генерируем последовательность для файла 2
-max_index = str(file2_elements.split().index(str(max([int(x) for x in file2_elements.split()]))))  # максимальный индекс файла 2
+max_index = str(
+    file2_elements.split().index(str(max([int(x) for x in file2_elements.split()]))))  # максимальный индекс файла 2
 all_elements += " " + file2_elements  # Все элементы двух файлов
 count_elements = len(all_elements.split())  # количество элементов
-elements_are_multiples_of_4 = " ".join([str(x) for x in all_elements.split()  # Элементы которые делятся на 4
+elements_are_multiples_of_4 = " ".join([str(x) for x in all_elements.split()  # Элементы, которые делятся на 4
                                         if int(x) % 4 == 0])
 file2.write(file2_elements)  # Запись в файл 2
 file2.close()  # закрывает файл
